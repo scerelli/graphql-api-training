b00a5f0f0dd6b3409265f829e8e7e16eb58482eb
Stefano Cerelli <hi@stefanocerelli.com>
2020-06-15 16:30:22 +0100

    Add training types and query

--- src/index.js
+++ src/index.js
@@ -53,16 +53,15 @@ const discountMockData = [
 // Type definitions define the "shape" of your data and specify
 // which ways the data can be fetched from the GraphQL server.
 const typeDefs = gql`
-  # Comments in GraphQL are defined with the hash (#) symbol.
-  # This "Book" type can be used in other type declarations.
-  type Book {
+  type Training {
     title: String
-    author: String
+    id: ID!
+    objectives: String
+    curriculum: String
   }
-  # The "Query" type is the root of all GraphQL queries.
-  # (A "Mutation" type will be covered later on.)
+
   type Query {
-    books: [Book]
+    trainings: [Training]
   }
 `;
 
@@ -70,7 +69,7 @@ const typeDefs = gql`
 // schema.  We'll retrieve books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books,
+    trainings: () => trainingMockData,
   },
 };
 
